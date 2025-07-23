ad8a1e382c69f0949c2b9f66ad1be028c02bb650
halfpointez <halfpointez@gmail.com>
2025-07-23 18:12:13 +0800

    windows

--- code/.vscode/note.md
+++
@@ -1,4 +0,0 @@
-git help //帮助
-git init //初始化本地仓库
-git add . //提交该目录下的更改
-git commit //确认提交
--- code/devc++/2.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main() {
-	int n, m;
-	cin >> n >> m;
-	cout<<"n是"<<n<<", m是"<<m<<endl;
-	return 0;
-}	
