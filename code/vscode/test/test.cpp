91e440deb8fc6d8bd70bddb0e63bca28f2b50915
halfpoint <3159584912@qq.com>
2025-08-12 16:06:14 +0800

    1

--- code/vscode/test/test.cpp
+++ code/vscode/test/test.cpp
@@ -1,38 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int T; // 总数据次数
-    cin>>T;
 
-    while (T--)
-    {
-        int n;//阶数
-        vector<int> f(n);
-        for (int i = 0; i < n;i++){
-            cin >> f[i];
-        }
-            int jvzheng[n][n]={0};
+class Person {
+ public:
+     int age;
+     Person(int age){
+         this->age = age;
+     }
 
-            for (int i = 0; i < n;i++){
-                vector<int> m;//用于判断每一行所需要的前置值是否满足
+     Person& add(Person &p){
+         this->age += p.age;
+         return *this;
+     }
+};
 
-                    for (int k = 0; k < f[i];k++){
-                        m.push_back(k);
-                    }
+void test01() {
+    Person p1(18);
+    cout << p1.age;
+}
 
-                for (int j = 0; j < n;j++){
-                    if()
+void test02(){
+    Person p1(10);
+    Person p2(10);
+    p2.add(p1).add(p1).add(p1);
+    cout << p2.age;
+}
 
 
-                }
-            }//对每一行进行检索
-
-            for (int i = 0; i < n;i++){
-                for (int j = 0; j < n;j++){
-
-                }
-            }//对每一列进行检索
-    }
-    
-    
+int main() {
+    test02();
 }
