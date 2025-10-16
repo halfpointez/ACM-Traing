267cbd7b77df68e92d4fa52d37dd331abbff75b5
halfpointez <halfpointez@gmail.com>
2025-10-16 19:14:16 +0800

    .

---
+++ code/洛谷/P2392_kkksc03考前临时抱佛脚.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+int main() {
+  int time;
+  int s1, s2, s3, s4;
+  cin >> s1 >> s2 >> s3 >> s4;
+  vector<int> A(s1), B(s2), C(s3), D(s4);
+
+  for (auto& i : A) {
+    cin >> i;
+  }
+    for(auto &i:B){
+      cin >> i;
+    }
+    for(auto &i:C){
+      cin >> i;
+    }
+    for(auto &i:D){
+      cin >> i;
+    }
+
+    while (D.)
+    {
+        
+    }
+    
+}
