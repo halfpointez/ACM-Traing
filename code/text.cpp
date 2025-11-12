1540d46a5de073df1cbc7472084c3d3b685295bd
halfpointez <halfpointez@gmail.com>
2025-11-12 20:47:01 +0800

    1

--- code/text.cpp
+++ code/text.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Example {
+public:
+    Example() {
+
+    }
     
+    static int A;
+    int B;
+    
+    Example &add(const Example &e) {
+        this->B+=e.B;
+        return *this;
+    }
+    ~Example() {
+
+    }
+};
+int Example::A=100;
+int main() {
+    Example e1,e2;
+    e1.B=10;
+    e2.B=10;
+    e1.add(e2).add(e2).add(e2);
+    cout<<e1.B;
+}
